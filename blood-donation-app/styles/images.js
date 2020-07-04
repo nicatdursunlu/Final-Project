525792156ab668fa4f48c7adc37853e89bfdfb8d
aydan-g <aydangozalova@gmail.com>
2020-07-04 15:23:33 +0400

    styles, images,icons and fonts changed

--- blood-donation-app/styles/images.js
+++ blood-donation-app/styles/images.js
@@ -1,19 +1,25 @@
-import bubbles from "../assets/images/bubbles.png";
-import welcomeImgBG from "../assets/images/welcomeImgBG.jpg";
-import logInImgBG from "../assets/images/logInImgBG.png";
-
-import femaleAvatar from "../assets/images/female_avatar.png";
+import bubbles from "../assets/images/bubbles.jpg";
 import drawerImg from "../assets/images/drawerImg.png";
-import ellipse from "../assets/images/ellipse.png";
-
+import femaleAvatar from "../assets/images/female_avatar.png";
 import profileBG from "../assets/images/profileBG.png";
+import img1 from "./../assets/images/img1.png";
+import img2 from "./../assets/images/img2.png";
+import img3 from "./../assets/images/img3.png";
+import img4 from "./../assets/images/img4.png";
+import img5 from "./../assets/images/img5.png";
+import img6 from "./../assets/images/img6.png";
+import img7 from "./../assets/images/img7.png";
 
 export const IMAGES = {
   bubbles,
-  welcomeImgBG,
-  logInImgBG,
   femaleAvatar,
   drawerImg,
-  ellipse,
   profileBG,
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
 };
