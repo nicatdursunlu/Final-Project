e1880188f39dca3e152b098164954b242aa8561e
firaqurbanova <firaqurbanovaa@gmail.com>
2020-07-13 23:40:19 +0400

    some screen elements redefined

--- blood-donation-app/screens/AuthScreen/index.js
+++ blood-donation-app/screens/AuthScreen/index.js
@@ -1,4 +1,3 @@
 export { WelcomeScreen } from "./WelcomeScreen";
 export { LogInScreen } from "./LogInScreen";
 export { SignUpScreen } from "./SignUpScreen";
-export { ForgetPasswordScreen } from "./ForgetPasswordScreen";
